8baac8a2a849d8804fbd86bbfcfb446434420d1d
selly <santoso.selly@gmail.com>
2019-04-24 16:14:13 +1000

    add readme.md

--- zodiac.rb
+++ zodiac.rb
@@ -1,23 +1,36 @@
-%w{colorize zodiac}.each { |x| require x }
+#Zodiac.rb
+%w{colorize zodiac httparty tty-prompt ./api.rb ./module.rb ./utility.rb ruby_emoji}.each { |x| require x }
 
-puts("Enter Date of birth (eg. 23/09/1994)")
-user_dob = gets().strip
 
-arr = user_dob.split("/")
-# arr.map { |x| x = x.to_i }
-#1901 +
-puts(DateTime.new(arr[2].to_i, arr[1].to_i, arr[0].to_i).zodiac_sign.to_s)
+prompt = TTY::Prompt.new    #initiate an object from tty-prompt class to use to display user selections
 
-chinese = [[1909, "Rooster"], [1910, "Dog"],
-           [1911, "Pig"], [1912, "Rat"],
-           [1901, "Ox"], [1902, "Tiger"],
-           [1903, "Rabbit"], [1904, "Dragon"],
-           [1905, "Snake"], [1906, "Horse"],
-           [1907, "Goat"], [1908, "Monkey"]]
+date, month, year = Zodiac::get_user_dob(prompt) # this method prompt user and return date,month,year resp.
 
-chinese.each { |item|
-  result = (item[0] - arr[2].to_i) % 12
-  if (result == 0)
-    puts(item[1])
+zodiac = Zodiac::get_wzodiac(date, month, year) # calling get_wzodiac from module.rb
+puts(Utility::random_color("\nYour Zodiac is ") + Utility::random_color(zodiac).underline.bold)
+
+# to take user selection and display user selection
+
+choice = ""
+while (choice != "quit")
+  choice = prompt.select("\nWhat whould you like to know about your " +
+                         RubyEmoji.parse("#{emoji(zodiac.downcase.to_sym)}").yellow + " " + # need RubyEmoji gem
+                         "#{zodiac}".green + " " + RubyEmoji.parse("#{emoji(zodiac.downcase.to_sym)}").yellow +
+                         +"\nPlease select?".blue,
+                         ["Learn", "Match Zodiac", "Horoscope", "Chinese Zodiac", "Learn Other Zodiac", "quit"])
+  case choice
+  when "Learn"
+    Zodiac::learn_zodiac(zodiac, prompt) # calling function from module.rb
+  when "Match Zodiac"
+    Zodiac::match_zodiac(zodiac, prompt)
+  when "Horoscope"
+    puts("Coming Soon")
+  when "Chinese Zodiac"
+    puts("Chinese Zodiac : " + Zodiac::get_czodiac(year).yellow)
+  when "Learn Other Zodiac"
+    zodiac = Zodiac::change_zodiac(prompt)
+  when "quit"
+    system "clear"
+    break
   end
-}
+end
